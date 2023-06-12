044a59ac8074d718039ea6232d659b25021f0f5e
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-06-11 20:28:58 -0400

    Added add server button to servers component, using one-way data binding via property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: '.app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent {
-
+  allowNewServer = false;
+  //After 2 seconds, allowNewServer will be true
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 }
