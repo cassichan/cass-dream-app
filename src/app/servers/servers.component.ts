c2a7ff886595d91a210075173ae358dde23baca1
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-06-15 14:57:40 -0400

    delete comments

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -17,7 +17,7 @@ export class ServersComponent {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = 'Server was created!' + 'Name is:' + this.serverName;
   }
   onUpdateServerName(event: any) {
     console.log(event);
