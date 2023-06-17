5a33a6b25b4ef882f60fb3fb00f66d2967bd6829
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-06-17 15:56:52 -0400

    create details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ServersComponent } from './servers/servers.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { FormsModule } from '@angular/forms';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { FormsModule } from '@angular/forms';
     ServersComponent,
     WarningAlertComponent,
     SuccessAlertComponent,
+    DetailsComponent,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
