c2c4d7dc9741bd53d1d3a7032780fef40c255944
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-06-17 19:56:40 -0400

    remove comments, create array of values for button clicks, loop through and sum to increment total number of clicks, used string interpolation to output number of times user clicks button

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -7,8 +7,24 @@ import { Component } from '@angular/core';
 })
 export class DetailsComponent {
   detailsVisible = false;
+  buttonClicks = [];
+  totalButtonClicks = 0;
+
+  clickButton() {
+    this.addButtonClicks();
+    this.displayDetails();
+  }
 
   displayDetails() {
     this.detailsVisible = !this.detailsVisible;
+  }
+
+  addButtonClicks() {
+    this.buttonClicks.push(this.buttonClicks.length + 1);
+    console.log(this.buttonClicks);
+    for (let i = 0; i < this.buttonClicks.length; i++) {
+      this.totalButtonClicks = +this.buttonClicks[i];
+    }
+    return this.totalButtonClicks;
   }
 }
