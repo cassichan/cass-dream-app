2d74d78c94a4f0445a0d29da5f13cf1bad593f49
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-07-04 14:50:22 -0400

    feature: add new server, list servers, and remove servers by index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styles: [
     `
-  h3 {
-    color: dodgerblue;
-  }`,
+      h3 {
+        color: dodgerblue;
+      }
+    `,
   ],
 })
-export class AppComponent {}
+export class AppComponent {
+  servers = [];
+
+  onAddServer() {
+    this.servers.push('Another Server');
+  }
+
+  onRemoveServer(id: number) {
+    const position = id;
+    this.servers.splice(position, 1);
+  }
+}
