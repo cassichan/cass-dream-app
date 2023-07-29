0e8837c9dc3ab95bcace9cc2c5d72d740d328ce4
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-07-29 16:40:04 -0400

    move  p tag styles to server-element to demonstrate view encapsulation, and demonstrate how to override (p styles will be applied globally)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
- styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  serverElements = [{type: 'server', name: 'Test-server', content: 'Just a test!'}];
+  serverElements = [
+    { type: 'server', name: 'Test-server', content: 'Just a test!' },
+  ];
 
   //Get data in this method, which is called  once our event is emitted from server-control component.
-  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
     this.serverElements.push({
       type: 'server',
       name: serverData.serverName,
@@ -17,12 +19,14 @@ export class AppComponent {
     });
   }
 
-  onBlueprintAdded(blueprintData: {blueprintName: string, blueprintContent: string}) {
+  onBlueprintAdded(blueprintData: {
+    blueprintName: string;
+    blueprintContent: string;
+  }) {
     this.serverElements.push({
       type: 'blueprint',
       name: blueprintData.blueprintName,
       content: blueprintData.blueprintContent,
     });
   }
-
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.None //This is to disable the default encapsulation of Angular.
 })
 export class ServerElementComponent {
   @Input('serverElement') element: {
