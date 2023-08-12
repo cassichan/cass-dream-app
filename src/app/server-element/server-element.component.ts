4214bb9e448f9c92f8a0d5749be54063f964e795
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-08-12 10:35:14 -0400

    tap into ngOnChanges, access changes object

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges {
   @Input('serverElement') element: {
     type: string;
     name: string;
@@ -14,6 +14,12 @@ export class ServerElementComponent implements OnInit {
 
   constructor() {
     console.log('constructor called');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called');
+    console.log(changes);
+    //object with current value, previousValue, firstChange of Input bound property
   }
   ngOnInit() {
     console.log('ngOnInit called');
